2ce3cfc4bfe10704aaaa985711c47ec6d3eea664
masonmil <masonmil@umich.edu>
2026-03-09 18:59:45 -0400

    #1 clean up impl, need to fix test file, add serialization soon (trust)

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,28 +1,85 @@
 #include "Database.hpp"
 
-std::ostream& operator<<(std::ostream& os, Board board) {
+#include <algorithm>
+#include <stdexcept>
+
+uint64_t Entry::entries = 0;
+
+ClimbingLog::ClimbingLog() { entries.reserve(512); }
+
+ClimbingLog::~ClimbingLog() = default;
+
+uint64_t ClimbingLog::addEntry(const Entry& entry) {
+  entries.emplace_back(entry);
+  return entries.back().entryID;
+}
+
+void ClimbingLog::removeEntry(const uint64_t entryID) {
+  const auto it = findByID(entryID);
+  if (it == entries.end())
+    throw std::out_of_range("removeEntry: entryID " + std::to_string(entryID) +
+                            " not found");
+  entries.erase(it);
+}
+
+Entry& ClimbingLog::getEntry(const uint64_t entryID) {
+  const auto it = findByID(entryID);
+  if (it == entries.end())
+    throw std::out_of_range("getEntry: entryID " + std::to_string(entryID) +
+                            " not found");
+  return *it;
+}
+
+const Entry& ClimbingLog::getEntry(const uint64_t entryID) const {
+  const auto it = findByID(entryID);
+  if (it == entries.end())
+    throw std::out_of_range("getEntry: entryID " + std::to_string(entryID) +
+                            " not found");
+  return *it;
+}
+
+void ClimbingLog::setEntry(const uint64_t entryID, Entry entry) {
+  const auto it = findByID(entryID);
+  if (it == entries.end())
+    throw std::out_of_range("setEntry: entryID " + std::to_string(entryID) +
+                            " not found");
+  entry.entryID = entryID;
+  *it = std::move(entry);
+}
+
+std::vector<Entry>::iterator ClimbingLog::findByID(uint64_t entryID) {
+  return std::ranges::find_if(
+      entries, [entryID](const Entry& e) { return e.entryID == entryID; });
+}
+
+std::vector<Entry>::const_iterator ClimbingLog::findByID(
+    uint64_t entryID) const {
+  return std::ranges::find_if(
+      entries, [entryID](const Entry& e) { return e.entryID == entryID; });
+}
+
+std::ostream& operator<<(std::ostream& os, const Board board) {
   switch (board) {
     case Board::MB2019:
       os << "MoonBoard 2019";
       break;
-
     default:
       os << "N/A";
   }
-
   return os;
 }
 
-std::ostream& operator<<(std::ostream& os, Grade grade) {
+std::ostream& operator<<(std::ostream& os, const Grade grade) {
   switch (grade) {
     case Grade::V3:
       os << "6a+/V3";
       break;
-
+    case Grade::V4:
+      os << "6b/V4";
+      break;
     default:
       os << "Ungraded";
   }
-
   return os;
 }
 
@@ -30,35 +87,22 @@ std::ostream& operator<<(std::ostream& os, const Entry& entry) {
   os << "\t" << entry.date << "\n";
   os << "\t" << entry.board << " at " << entry.incline << " degrees\n";
   os << "\t" << entry.name << " (" << entry.grade << ")\n";
-  if (entry.sent && entry.attempts == 1) os << "\tFlashed!\n";
-  if (entry.sent)
-    os << "\tSent, " << entry.attempts << " attempts\n";
+
+  if (entry.sent && entry.attempts == 1)
+    os << "\tFlashed!\n";
+  else if (entry.sent)
+    os << "\tSent in " << entry.attempts << " attempts\n";
   else
-    os << "\tProject, " << entry.attempts << " attempts\n";
+    os << "\tProject, " << entry.attempts << " attempts so far\n";
 
   return os;
 }
 
-Database::Database() { log.reserve(16); }
-
-Database::~Database() = default;
-
-void Database::addEntry(const Entry& entry) { log.emplace_back(entry); }
-
-void Database::removeEntry(const uint32_t entryID) {
-  // Combination of remove_if and erase?
-}
-
-std::ostream& operator<<(std::ostream& os, const Database& db) {
-  const std::vector<Entry>& log = db.log;
-  os << "Climbing Log\n";
-
-  for (size_t i = 0; i < log.size(); i++) {
-    const Entry& entry = log[i];
-
-    os << "[" << i << "]:\n";
-    os << entry << "\n";
+std::ostream& operator<<(std::ostream& os, const ClimbingLog& log) {
+  os << "Climbing Log (" << log.entries.size() << " entries)\n";
+  for (size_t i = 0; i < log.entries.size(); i++) {
+    os << "[" << i << "] (ID " << log.entries[i] << "):\n";
+    os << log.entries[i] << "\n";
   }
-
   return os;
 }
--- src/Database.hpp
+++ src/Database.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <chrono>
-#include <iostream>
+#include <ostream>
+#include <stdexcept>
 #include <string>
+#include <utility>
 #include <vector>
 
 enum Board {
@@ -13,12 +15,35 @@ std::ostream& operator<<(std::ostream& os, Board board);
 
 enum Grade {
   V3,
+  V4,
 };
 
 std::ostream& operator<<(std::ostream& os, Grade grade);
 
-struct Entry {
-  uint32_t entryID;
+class Entry {
+ public:
+  Entry(const std::chrono::year_month_day date, std::string name,
+        const Board board, const Grade grade, const uint32_t attempts,
+        const double incline, const bool sent)
+      : entryID(entries++),
+        date(date),
+        name(std::move(name)),
+        board(board),
+        grade(grade),
+        attempts(attempts),
+        incline(incline),
+        sent(sent) {}
+
+  [[nodiscard]] uint64_t getID() const { return entryID; }
+
+  friend std::ostream& operator<<(std::ostream& os, const Entry& entry);
+
+ private:
+  friend class ClimbingLog;
+
+  static uint64_t entries;
+
+  uint64_t entryID;
   std::chrono::year_month_day date;
   std::string name;
   Board board;
@@ -28,26 +53,29 @@ struct Entry {
   bool sent;
 };
 
-std::ostream& operator<<(std::ostream& os, const Entry& entry);
+class ClimbingLog {
+ public:
+  ClimbingLog();
+  ~ClimbingLog();
 
-class Database {
-public:
-  Database();
+  uint64_t addEntry(const Entry& entry);
 
-  void addEntry(const Entry& entry);
+  void removeEntry(uint64_t entryID);
 
-  void removeEntry(uint32_t entryID);
+  Entry& getEntry(uint64_t entryID);
+  [[nodiscard]] const Entry& getEntry(uint64_t entryID) const;
 
-  // TODO: addEntry overload takes Entry params, serializeLog, deserializeLog, readEntry, updateEntry
-  // TODO: some sort of iterator friend class?
+  void setEntry(uint64_t entryID, Entry entry);
 
-  ~Database();
+  [[nodiscard]] size_t size() const { return entries.size(); }
+  [[nodiscard]] bool empty() const { return entries.empty(); }
 
-  friend std::ostream& operator<<(std::ostream& os, const Database& db);
+  friend std::ostream& operator<<(std::ostream& os, const ClimbingLog& log);
 
+ private:
+  std::vector<Entry>::iterator findByID(uint64_t entryID);
+  [[nodiscard]] std::vector<Entry>::const_iterator findByID(
+      uint64_t entryID) const;
 
-private:
-  // TODO: Does std::vector make sense here?
-  std::vector<Entry> log;
-
+  std::vector<Entry> entries;
 };
